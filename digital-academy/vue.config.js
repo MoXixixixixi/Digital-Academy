d14889f2c0c12ca7d7625647dc649209ac3c0280
MoXixixixixi <3322027671@qq.com>
2024-08-03 21:50:40 +0800

    修改配置文件，完成并部署初版数产Unity

--- digital-academy/vue.config.js
+++ digital-academy/vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false //关闭语法检查
+  lintOnSave: false, //关闭语法检查
+  publicPath: './', // 基本路径
+  outputDir: 'dist', // 输出文件目录 默认值
+  assetsDir: './assets',//放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
+  indexPath: 'index.html',//打包生成的 index.html 文件的存储位置 默认值
 })
